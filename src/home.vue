838120575114b062656bd184af576234c6144fa8
张桥 <qiaoqiao10001@gmail.com>
2020-04-23 16:41:57 +0800

    add todos

--- src/home.vue
+++ src/home.vue
@@ -2,6 +2,15 @@
   <div>
     {{name}}
     <h2>vue 构建</h2>
+    使用webpack搭建开发环境
+    <h3>todo: </h3>
+    <ul>
+      <li>axios拦截器</li>
+      <li>cookie记录</li>
+      <li>UI组件库初始化</li>
+      <li>vuex初始化</li>
+      
+    </ul>
   </div>
 </template>
 
